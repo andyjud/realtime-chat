d1ba9f8e6d75acc9cb4a26e297481e713c739781
Andreas Jud <andreas.jud@gmail.com>
2024-03-28 13:30:20 +0800

    Create asgi.py

---
+++ asgi.py
@@ -0,0 +1,19 @@
+import os
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from channels.auth import AuthMiddlewareStack
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'a_core.settings')
+
+django_asgi_app = get_asgi_application()
+
+from a_rtchat import routing
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns))
+    ),
+})
