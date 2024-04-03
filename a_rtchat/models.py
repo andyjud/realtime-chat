9a0cf295b57a904aa57c0592744de495a90d66c2
Andreas Jud <andreas.jud@gmail.com>
2024-04-04 07:59:38 +0800

    Update models.py

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True)
+    users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True)
     
     def __str__(self):
         return self.group_name
