85608a701a1cdc9d655c4728b3c1d43437703dee
Andreas Jud <andreas.jud@gmail.com>
2024-04-04 07:59:16 +0800

    Update consumers.py

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -15,13 +15,22 @@ class ChatroomConsumer(WebsocketConsumer):
             self.chatroom_name, self.channel_name
         )
         
+        # add and update online users
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+        
         self.accept()
         
         
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name, self.channel_name
-        ) 
+        )
+        # remove and update online users
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count() 
         
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
@@ -49,3 +58,18 @@ class ChatroomConsumer(WebsocketConsumer):
         }
         html = render_to_string("a_rtchat/partials/chat_message_p.html", context=context)
         self.send(text_data=html)
+        
+        
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count() -1
+        
+        event = {
+            'type': 'online_count_handler',
+            'online_count': online_count
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+        
+    def online_count_handler(self, event):
+        online_count = event['online_count']
+        html = render_to_string("a_rtchat/partials/online_count.html", {'online_count' : online_count})
+        self.send(text_data=html) 
