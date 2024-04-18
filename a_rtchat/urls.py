d608501555f0e01d01ff6e8c765e453fc843068e
Andreas Jud <andreas.jud@gmail.com>
2024-04-18 12:30:41 +0800

    Update urls.py

--- a_rtchat/urls.py
+++ a_rtchat/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', chat_view, name="home"),
+    path('chat/<username>', get_or_create_chatroom, name="start-chat"),
+    path('chat/room/<chatroom_name>', chat_view, name="chatroom"),
 ]
