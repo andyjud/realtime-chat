c8be97ea18adedcbc3bb8276cfa397261809d1d9
Andreas Jud <andreas.jud@gmail.com>
2024-04-26 11:08:02 +0800

    Update models.py

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -4,6 +4,8 @@ import shortuuid
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True, default=shortuuid.uuid)
+    groupchat_name = models.CharField(max_length=128, null=True, blank=True)
+    admin = models.ForeignKey(User, related_name='groupchats', blank=True, null=True, on_delete=models.SET_NULL)
     users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True)
     members = models.ManyToManyField(User, related_name='chat_groups', blank=True)
     is_private = models.BooleanField(default=False)
