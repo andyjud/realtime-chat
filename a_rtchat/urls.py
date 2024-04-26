fccde028242dfe5f19cbe35a1221900487422014
Andreas Jud <andreas.jud@gmail.com>
2024-04-26 11:07:37 +0800

    Update urls.py

--- a_rtchat/urls.py
+++ a_rtchat/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', chat_view, name="home"),
     path('chat/<username>', get_or_create_chatroom, name="start-chat"),
     path('chat/room/<chatroom_name>', chat_view, name="chatroom"),
+    path('chat/new_groupchat/', create_groupchat, name="new-groupchat"),
+    path('chat/edit/<chatroom_name>', chatroom_edit_view, name="edit-chatroom"),
+    path('chat/delete/<chatroom_name>', chatroom_delete_view, name="chatroom-delete"),
+    path('chat/leave/<chatroom_name>', chatroom_leave_view, name="chatroom-leave"),
 ]
