e6788fbe2b29adeb1a97f7aaea547100fcb6c6b9
Andreas Jud <andreas.jud@gmail.com>
2024-04-26 11:15:30 +0800

    Update forms.py

--- a_rtchat/forms.py
+++ a_rtchat/forms.py
@@ -8,4 +8,30 @@ class ChatmessageCreateForm(ModelForm):
         fields = ['body']
         widgets = {
             'body' : forms.TextInput(attrs={'placeholder': 'Add message ...', 'class': 'p-4 text-black', 'maxlength' : '300', 'autofocus': True }),
-        } 
+        }
+        
+        
+class NewGroupForm(ModelForm):
+    class Meta:
+        model = ChatGroup
+        fields = ['groupchat_name']
+        widgets = {
+            'groupchat_name' : forms.TextInput(attrs={
+                'placeholder': 'Add name ...', 
+                'class': 'p-4 text-black', 
+                'maxlength' : '300', 
+                'autofocus': True,
+                }),
+        }
+        
+        
+class ChatRoomEditForm(ModelForm):
+    class Meta:
+        model = ChatGroup
+        fields = ['groupchat_name']
+        widgets = {
+            'groupchat_name' : forms.TextInput(attrs={
+                'class': 'p-4 text-xl font-bold mb-4', 
+                'maxlength' : '300', 
+                }),
+        }
