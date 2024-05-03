54011f4fefdf9530b46fd94bab46c2f5aec13372
Andreas Jud <andreas.jud@gmail.com>
2024-05-03 13:23:13 +0800

    Update routing.py

--- a_rtchat/routing.py
+++ a_rtchat/routing.py
@@ -3,4 +3,5 @@ from .consumers import *
 
 websocket_urlpatterns = [
     path("ws/chatroom/<chatroom_name>", ChatroomConsumer.as_asgi()),
+    path("ws/online-status/", OnlineStatusConsumer.as_asgi()),
 ]
