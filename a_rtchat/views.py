d933137707f476bd94dbf4a891b3983aa0bd5792
Andreas Jud <andreas.jud@gmail.com>
2024-05-10 14:56:43 +0800

    Update views.py

--- a_rtchat/views.py
+++ a_rtchat/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect 
 from django.contrib.auth.decorators import login_required
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from django.http import HttpResponse
 from django.contrib import messages
 from django.http import Http404
 from .models import *
@@ -145,3 +148,24 @@ def chatroom_leave_view(request, chatroom_name):
         chat_group.members.remove(request.user)
         messages.success(request, 'You left the Chat')
         return redirect('home')
+    
+    
+def chat_file_upload(request, chatroom_name):
+    chat_group = get_object_or_404(ChatGroup, group_name=chatroom_name)
+    
+    if request.htmx and request.FILES:
+        file = request.FILES['file']
+        message = GroupMessage.objects.create(
+            file = file,
+            author = request.user, 
+            group = chat_group,
+        )
+        channel_layer = get_channel_layer()
+        event = {
+            'type': 'message_handler',
+            'message_id': message.id,
+        }
+        async_to_sync(channel_layer.group_send)(
+            chatroom_name, event
+        )
+    return HttpResponse()
