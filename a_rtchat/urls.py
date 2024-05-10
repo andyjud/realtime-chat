5f9999d494a10d8e18a08965065955af4bc1b6e6
Andreas Jud <andreas.jud@gmail.com>
2024-05-10 14:57:05 +0800

    Update urls.py

--- a_rtchat/urls.py
+++ a_rtchat/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('chat/edit/<chatroom_name>', chatroom_edit_view, name="edit-chatroom"),
     path('chat/delete/<chatroom_name>', chatroom_delete_view, name="chatroom-delete"),
     path('chat/leave/<chatroom_name>', chatroom_leave_view, name="chatroom-leave"),
+    path('chat/fileupload/<chatroom_name>', chat_file_upload, name="chat-file-upload"),
 ]
