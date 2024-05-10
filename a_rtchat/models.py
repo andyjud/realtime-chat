7f86883cdc6bafe80fef44b151665285ed159312
Andreas Jud <andreas.jud@gmail.com>
2024-05-10 14:55:30 +0800

    Update models.py

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import shortuuid
+from PIL import Image
+import os
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=128, unique=True, default=shortuuid.uuid)
@@ -17,11 +19,31 @@ class ChatGroup(models.Model):
 class GroupMessage(models.Model):
     group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.CharField(max_length=300)
+    body = models.CharField(max_length=300, blank=True, null=True)
+    file = models.FileField(upload_to='files/', blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     
+    @property
+    def filename(self):
+        if self.file:
+            return os.path.basename(self.file.name)
+        else:
+            return None
+    
     def __str__(self):
-        return f'{self.author.username} : {self.body}'
+        if self.body:
+            return f'{self.author.username} : {self.body}'
+        elif self.file:
+            return f'{self.author.username} : {self.filename}'
     
     class Meta:
         ordering = ['-created']
+        
+    @property    
+    def is_image(self):
+        try:
+            image = Image.open(self.file) 
+            image.verify()
+            return True 
+        except:
+            return False
