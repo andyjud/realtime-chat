bcb04096a40b5c2ce882af6edb0fe4c76666132b
Andreas Jud <andreas.jud@gmail.com>
2024-05-22 11:14:18 +0800

    Update models.py

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 import os
 
 class ChatGroup(models.Model):
-    group_name = models.CharField(max_length=128, unique=True, default=shortuuid.uuid)
+    group_name = models.CharField(max_length=128, unique=True, blank=True)
     groupchat_name = models.CharField(max_length=128, null=True, blank=True)
     admin = models.ForeignKey(User, related_name='groupchats', blank=True, null=True, on_delete=models.SET_NULL)
     users_online = models.ManyToManyField(User, related_name='online_in_groups', blank=True)
@@ -14,6 +14,11 @@ class ChatGroup(models.Model):
     
     def __str__(self):
         return self.group_name
+
+    def save(self, *args, **kwargs):
+        if not self.group_name:
+            self.group_name = shortuuid.uuid()
+        super().save(*args, **kwargs)
     
     
 class GroupMessage(models.Model):
